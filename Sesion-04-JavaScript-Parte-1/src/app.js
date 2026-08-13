7c2877056d0fe27e217f1ecab27353faa47bd9db
Cristofer Antonio Berganza Portillo <antoniocristofer577@gmail.com>
2026-08-13 13:39:52 -0600

    add toggle and filters

--- Sesion-04-JavaScript-Parte-1/src/app.js
+++ Sesion-04-JavaScript-Parte-1/src/app.js
@@ -70,8 +70,15 @@ export function eliminarTarea(id) {
  * @returns {boolean}
  */
 export function toggleTarea(id) {
-    // TODO: recorrer `tareas` y cambiar `completada` de la que coincida.
-    // Devuelve true si la encontró.
+    const tarea = tareas.find(tarea => tarea.id === id);
+
+    if(!tarea){
+        return false;
+    }
+
+    tarea.completada = !tarea.completada;
+
+    return true;
 }
 
 /**
@@ -80,7 +87,15 @@ export function toggleTarea(id) {
  * @returns {Array}
  */
 export function filtrarTareas(filtro) {
-    // TODO: implementar la lógica de filtrado.
+    if(filtro === "pendientes"){
+        return tareas.filter(tarea => !tarea.completada);
+    }
+
+    if(filtro === "completadas"){
+        return tareas.filter(tarea => tarea.completada);
+    }
+
+    return tareas;
 }
 
 /**
